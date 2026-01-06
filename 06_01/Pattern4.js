48f3111c65f8da71a9f081fa707bd376bbbc27ed
krishna <krishnaptl0414@gmail.com>
2026-01-06 14:00:08 +0530

    comment add

--- 06_01/Pattern4.js
+++ 06_01/Pattern4.js
@@ -9,5 +9,12 @@
 
 */
 
-
-
+let n = 5;
+let pattern = "";
+for (let i = n; i >= 1; i--) {
+    for (let j = 1; j <= i; j++) {
+        pattern = pattern + j;
+    }
+    pattern = pattern + "\n";
+}
+console.log(pattern);
--- 06_01/Pattern6.js
+++ 06_01/Pattern6.js
@@ -1,4 +1,16 @@
-let n = 4;
+/*
+
+6.Print Pattern program for 
+
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+
+*/
+
+let n = 5;
 let num = 1;
 let pattern = "";
 for (let i = 1; i <= n; i++) {
@@ -8,3 +20,18 @@ for (let i = 1; i <= n; i++) {
     pattern = pattern + "\n";
 }
 console.log(pattern);
+
+/* num=1
+// pattern="" i=1;1<=5
+// j=1;1<1
+// pattern="" +1   
+// j=2;2<1   //1
+
+/*num=2 paattern=""
+i=2 ;2<=5
+j=1;1<2
+pattern=""+2
+j=2;2<=2
+pattern="2"+3
+ j=3;3<2    //23
+ */
